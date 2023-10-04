3558aeec83a84206476efdbd9bc7d7cb71401a69
Nicole <nicolesstarling02@gmail.com>
2023-10-04 14:53:33 -0300

    Rotas de aplicação e Alunos da API

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Login() {
+export default function Aluno() {
   return (
     <Container>
-      <h1>LOGIN</h1>
+      <h1>ALUNO</h1>
     </Container>
   );
 }
